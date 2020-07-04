d29b06e277035315fd77b99020d9129d6b412089
Bogdan Ungureanu <49443238+daemoohn@users.noreply.github.com>
2020-07-05 00:19:29 +0300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,9 +4,33 @@
 git clone https://gitlab.xiph.org/xiph/ogg.git
 
 # configureOgg.sh
+
 cd ogg
 if [ ! -f configure ]; then
   # generate configuration script
   sed -i.bak 's/$srcdir\/configure/#/' autogen.sh
   ./autogen.sh
 fi
+
+# compileOggJs.sh
+
+# set up the build directory
+mkdir -p build
+cd build
+
+mkdir -p js
+cd js
+
+mkdir -p root
+mkdir -p libogg
+cd libogg
+
+# finally, run configuration script
+emconfigure ../../../libogg/configure \
+    --prefix="$dir/build/js/root" \
+    --disable-shared \
+|| exit 1
+
+# compile libogg
+emmake make -j4 || exit 1
+emmake make install || exit 1

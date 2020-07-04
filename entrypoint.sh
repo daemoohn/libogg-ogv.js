bb703a43418bfb61702863edfa29ffa3df35eee1
Bogdan Ungureanu <49443238+daemoohn@users.noreply.github.com>
2020-07-04 22:44:57 +0300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,2 +1,8 @@
 #!/bin/sh
-echo "Hello world"
+git clone https://gitlab.xiph.org/xiph/ogg.git
+cd ogg
+if [ ! -f configure ]; then
+  # generate configuration script
+  sed -i.bak 's/$srcdir\/configure/#/' autogen.sh
+  ./autogen.sh
+fi
